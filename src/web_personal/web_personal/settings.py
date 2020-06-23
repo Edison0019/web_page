7552232c79886611b081b6cfab58663439d4af54
edison0019 <edison_0019@hotmail.com>
2020-06-22 23:48:31 -0400

    new changes made to the portfolio web design

--- src/web_personal/web_personal/settings.py
+++ src/web_personal/web_personal/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'portfolio',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'EST'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
